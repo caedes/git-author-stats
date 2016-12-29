e64fbe6ff16b43f65d38341d06b28a43d22fbcca
caedes <laurentromain@gmail.com>
2016-12-29 18:09:18 +0100

    Refactor adding git-author-stats module

---
+++ src/git-author-stats/index.js
@@ -0,0 +1,51 @@
+import childProcess from 'child_process';
+import compact from 'lodash/compact';
+import fromPairs from 'lodash/fromPairs';
+import orderBy from 'lodash/orderBy';
+import reduce from 'lodash/reduce';
+import reverse from 'lodash/reverse';
+
+export default () => {
+  const execSync = childProcess.execSync;
+
+  const commitsCommand = 'git shortlog -s -n --all --no-merges';
+  const commitsCommandResult = execSync(commitsCommand).toString('utf8');
+  const commitStats = compact(commitsCommandResult.split('\n'));
+
+  const commitStatsPerAuthors = fromPairs(commitStats.map(stat => (
+    reverse(stat.split('\t'))
+  )));
+
+  const authorsCommand = "git log --format='%aN' | sort -u";
+  const authorsCommandResult = execSync(authorsCommand).toString('utf8');
+  const authors = compact(authorsCommandResult.split('\n'));
+
+  const stats = authors.map((author) => {
+    const commits = parseInt(commitStatsPerAuthors[author], 10) || 0;
+
+    const lineStatsCommand = `git log --no-merges --author="${author}" --pretty=tformat: --numstat`;
+    const lineStatsCommandResult = execSync(lineStatsCommand).toString('utf8');
+    const lineStats = lineStatsCommandResult.split('\n');
+    const lineStatsResult = lineStats.map(lineStat => (
+      lineStat.split('\t')
+    ));
+    const addedStatsResult = compact(lineStatsResult.map(lineStatResult => (
+      parseInt(lineStatResult[0], 10)
+    )));
+    const removedStatsResult = compact(lineStatsResult.map(lineStatResult => (
+      parseInt(lineStatResult[1], 10)
+    )));
+
+    const added = reduce(addedStatsResult, (a, b) => (a + b), 0);
+    const removed = reduce(removedStatsResult, (a, b) => (a + b), 0);
+
+    return {
+      author,
+      commits,
+      added,
+      removed,
+    };
+  });
+
+  return orderBy(stats, ['added', 'removed'], ['desc', 'desc']);
+};
