9bae5224e36e6fc8cecc928e4dc0d1a2aa880f41
caedes <laurentromain@gmail.com>
2016-12-29 15:12:19 +0100

    Add dynamic authors

--- index.js
+++ index.js
@@ -17,69 +17,31 @@ const commitStatsPerAuthors = fromPairs(commitStats.map(stat => (
   reverse(stat.split('\t'))
 )));
 
-const authors = [
-  {
-    names: [
-      'caedes',
-    ],
-  },
-  {
-    names: [
-      'maxime.degenne',
-      'CDBDX\\maxime.degenne',
-      'U-CDBDX\\maxime.degenne',
-    ],
-  },
-  {
-    names: [
-      'Pierre Nedelec',
-    ],
-  },
-  {
-    names: [
-      'pascal.maria',
-    ],
-  },
-  {
-    names: [
-      'Adrien Fillon',
-    ],
-  },
-];
+const authorsCommand = "git log --format='%aN' | sort -u";
+const authorsCommandResult = execSync(authorsCommand).toString('utf8');
+const authors = compact(authorsCommandResult.split('\n'));
 
 const stats = authors.map((author) => {
-  const authorStats = author.names.map((name) => {
-    const commits = parseInt(commitStatsPerAuthors[name], 10) || 0;
+  const commits = parseInt(commitStatsPerAuthors[author], 10) || 0;
 
-    const lineStatsCommand = `git log --no-merges --author="${name}" --pretty=tformat: --numstat`;
-    const lineStatsCommandResult = execSync(lineStatsCommand).toString('utf8');
-    const lineStats = lineStatsCommandResult.split('\n');
-    const lineStatsResult = lineStats.map(lineStat => (
-      lineStat.split('\t')
-    ));
-    const addedStatsResult = compact(lineStatsResult.map(lineStatResult => (
-      parseInt(lineStatResult[0], 10)
-    )));
-    const removedStatsResult = compact(lineStatsResult.map(lineStatResult => (
-      parseInt(lineStatResult[1], 10)
-    )));
+  const lineStatsCommand = `git log --no-merges --author="${author}" --pretty=tformat: --numstat`;
+  const lineStatsCommandResult = execSync(lineStatsCommand).toString('utf8');
+  const lineStats = lineStatsCommandResult.split('\n');
+  const lineStatsResult = lineStats.map(lineStat => (
+    lineStat.split('\t')
+  ));
+  const addedStatsResult = compact(lineStatsResult.map(lineStatResult => (
+    parseInt(lineStatResult[0], 10)
+  )));
+  const removedStatsResult = compact(lineStatsResult.map(lineStatResult => (
+    parseInt(lineStatResult[1], 10)
+  )));
 
-    const added = reduce(addedStatsResult, (a, b) => (a + b), 0);
-    const removed = reduce(removedStatsResult, (a, b) => (a + b), 0);
-
-    return {
-      commits,
-      added,
-      removed,
-    };
-  });
-
-  const commits = reduce(authorStats.map(stat => stat.commits), (a, b) => (a + b), 0);
-  const added = reduce(authorStats.map(stat => stat.added), (a, b) => (a + b), 0);
-  const removed = reduce(authorStats.map(stat => stat.removed), (a, b) => (a + b), 0);
+  const added = reduce(addedStatsResult, (a, b) => (a + b), 0);
+  const removed = reduce(removedStatsResult, (a, b) => (a + b), 0);
 
   return {
-    author: author.names[0],
+    author,
     commits,
     added,
     removed,
